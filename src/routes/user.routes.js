fb8ad24078afcc6d146e555fe3fa190968ca43df
[Ashwini kumar] <[ashwinikumar88229@gmail.com]>
2025-07-15 22:37:45 +0530

    feat: implement user authentication with login, logout, and token refresh functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser
+    , loginUser, 
+    logoutUser,
+     refreshAccessToken }
+     
+     
+
+from "../controllers/user.controller.js";
+ import {verifyJWT} from "../middlewares/auth.middleware.js";
 import {upload} from "../middlewares/multer.middleware.js"
 const router = Router();
 router.route("/register").post(
@@ -16,5 +24,12 @@ router.route("/register").post(
     ]),
     registerUser
     )
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post( refreshAccessToken)
 
 export default router;
